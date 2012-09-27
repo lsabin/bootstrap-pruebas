45e7fdd2e34d55ab902ed1f8db32438217afad6b
Leandro <sabin.leandro@gmail.com>
2012-09-27 12:45:24 +0200

    mejoras validacion

---
+++ js/script-validacion.js
@@ -0,0 +1,45 @@
+// 
+//	jQuery Validate example script
+//
+//	Prepared by David Cochran
+//	
+//	Free for your use -- No warranties, no guarantees!
+//
+
+$(document).ready(function(){
+
+	// Validate
+	// http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+	// http://docs.jquery.com/Plugins/Validation/
+	// http://docs.jquery.com/Plugins/Validation/validate#toptions
+	
+		$('#contact-form').validate({
+	    rules: {
+	      nombre: {
+	        minlength: 2,
+	        required: true
+	      },
+	      correo: {
+	        required: true,
+	        email: true
+	      },
+	      apellido: {
+	      	minlength: 2,
+	        required: true
+	      },
+	      ciudad: {
+	        minlength: 2,
+	        required: true
+	      }
+	    },
+	    highlight: function(label) {
+	    	$(label).closest('.control-group').addClass('error');
+	    },
+	    success: function(label) {
+	    	label
+	    		.text('OK!').addClass('valid')
+	    		.closest('.control-group').addClass('success');
+	    }
+	  });
+	  
+}); // end document.ready
